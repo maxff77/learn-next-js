e3d4f322614ebcb56aca4dae7e3a5fbccc0f939a
Richard <maxtrooff77@gmail.com>
2024-02-05 09:26:58 -0600

    Primera versión del proyecto antes de implementar la base de datos

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
+const lusitana = Lusitana({ subsets: ['latin'], weight: '700' });
+export { inter, lusitana };
