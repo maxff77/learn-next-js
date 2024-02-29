944b66e13a599323a40e38f1e54154bf81a41a07
Richard <maxtrooff77@gmail.com>
2024-02-28 18:15:12 -0600

    Se eliminó el error al intentar eliminar una factura

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -107,7 +107,7 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {
