2686590bf7eb240a9d251ed7a998ef55b8d9e730
Richard <maxtrooff77@gmail.com>
2024-03-11 10:59:58 -0600

    Prueba en base de datos

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -134,3 +134,4 @@ export async function authenticate(
         throw error;
     }
 }
+
--- auth.ts
+++ auth.ts
@@ -5,6 +5,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+
+// Obtiene en la base de datos el usuario que coincide on el email de la base de datos
  
 async function getUser(email: string): Promise<User | undefined> {
   try {
@@ -22,12 +24,16 @@ export const { auth, signIn, signOut } = NextAuth({
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
- 
+        .object({ email: z.string().email(), password: z.string().min(6) })
+        .safeParse(credentials);
+        
+        console.log(parsedCredentials)
+        
           if (parsedCredentials.success) {
             const { email, password } = parsedCredentials.data;
             const user = await getUser(email);
+            // console.log(email)
+            // console.log(password)
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
    
